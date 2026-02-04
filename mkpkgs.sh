470938f6b3d6998acd78fa88a97009fc8c7efa55
babywolf <fgsoftwarestudio@gmail.com>
2026-02-04 18:32:35 +0000

    - improving mkpkgs.sh - adding kando

--- mkpkgs.sh
+++ mkpkgs.sh
@@ -3,9 +3,12 @@
 su builder << 'EOF'
 gpg --recv-keys E25D679AF73C6D2F
 mkdir -p "/profile/localrepo" && chmod -R 777 "/profile/localrepo"
-cd /profile/packages/wlogout
-makepkg -s -f --noconfirm
+find /profile/packages -mindepth 1 -maxdepth 1 -type d | while read folder; do
+    echo "Processing folder: $folder"
+    cd "$folder"
+    makepkg -s -f --noconfirm
 cp *.pkg.tar.zst /profile/localrepo/
+done
 EOF
 
 cd /profile/localrepo
